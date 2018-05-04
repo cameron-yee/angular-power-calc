891ddb6a1fb2487d59e18bd1144f6c8310266c32
cameron-yee <yee.camero23@gmail.com>
2018-05-04 09:27:52 -0600

    Dropdown text selection working

--- src/app/dropdown/dropdown.component.ts
+++ src/app/dropdown/dropdown.component.ts
@@ -20,41 +20,18 @@ export class DropdownComponent implements AfterViewInit {
   }
 
   ngAfterViewInit() {
-    let setSelection: any = (item_id) => {
-      console.log('item_id: ' + item_id);
-      let item: any = document.getElementById(item_id);
-      console.log('item: ' + item);
-      let item_text: string = item.text;
-      console.log('item_text: ' + item_text);
-      let span_id: string = item_id.slice(0, -1) + 'span';
-      console.log('span_id: ' + span_id);
-      let span: any = document.getElementById(span_id);
-      console.log('span: ' + span);
-      span.text = item_text;
+    let setSelection: any = (item_text) => {
+      this.textspan.nativeElement.innerText = item_text;
     }
-
-    let test: any = () => {
-      console.log(1 + 1);
-    }
-
-    console.log(this.textspan);
-    // @ViewChild(id + '-span');
 
     for(let i = 0; i < this.dropdown_items_query.length; i++) {
-      this.dropdown_items_query.toArray()[i].nativeElement.addEventListener('click', test);
-      //setSelection(this.dropdown_items_query.toArray()[i].nativeElement.id)
+      let item_text = this.dropdown_items_query.toArray()[i].nativeElement.innerText;
+      this.dropdown_items_query.toArray()[i].nativeElement.addEventListener('click', function() {setSelection(item_text)});
     }
 
     let dropdown_items: Array<any> = [];
     for(let i = 0; i < this.dropdown_items_query.length; i++) {
       let item_id = this.dropdown_items_query.toArray()[i].nativeElement.id;
-      console.log(item_id);
-      // dropdown_items.push(item_id);
-      // let item = document.getElementById(item_id);
-      // item.addEventListener('click', setSelection(item))
     }
-    console.log(dropdown_items);
-
-      
   }
 }
