d9bb6418f78156f8b20adaa7995fc61adb0ee241
cameron-yee <yee.camero23@gmail.com>
2018-06-04 15:50:48 -0600

    Fixed vulnerable dependency in package-lock.json, Added function to scroll window to top when contact route is clicked

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  @ViewChild('contact') contact: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.contact.nativeElement.addEventListener('click', function() {window.scroll(0,0)});
   }
 
 }
