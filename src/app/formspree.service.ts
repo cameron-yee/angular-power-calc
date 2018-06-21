7f390ece57575ef2d07319e7e9246d21966ddedf
Jonathan Nickerson <jonathan.s.nickerson@gmail.com>
2018-06-21 09:33:19 -0600

    Trying to fix formspree 'empty form error'.

--- src/app/formspree.service.ts
+++ src/app/formspree.service.ts
@@ -13,7 +13,7 @@ export class FormspreeService {
   private email: string;
   private firstName: string;
   private lastName: string;
-  private message: string = `Project: POW2\n\nMessage from ${ this.firstName } ${ this.lastName } \n\n ${ this.message }`;
+  private message: string = `Project: POWER ANALYSIS PARAMETERS\n\nMESSAGE FROM: ${ this.lastName }, ${ this.firstName }\n\nREPLY TO: ${ this.email }\n\nMESSAGE:\n\n${ this.message }`;
 
   //NEED GOLD ACCOUNT TO AJAX POST
   private url: string = 'https://formspree.io/mdzzqblm';
@@ -25,15 +25,17 @@ export class FormspreeService {
   getStatus = () => { return this.status };
 
   postData = () => {
-    const data =  {
-      email: this.email,
-      subject: 'MESSAGE FROM POWER ANALYSIS PARAMETERS WEBSITE: ' + this.lastName + ", " + this.firstName,
-      message: 'Message from: ' + this.lastName + ", " + this.firstName + '\n\n' + this.message
-    }
+    // let data =  {
+    //   name: this.lastName + ', ' + this.firstName,
+    //   email: this.email,
+    //   message: this.message
+    // }
+
+    let data = `name=${this.lastName + ', ' + this.firstName}&email=${this.email}&message=${this.message}`;
 
     this.http.post(
       this.url,
-      JSON.stringify(data),
+      data,
       {headers: new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded'})}
     )
       .subscribe(res => {
